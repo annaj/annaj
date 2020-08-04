adae2bb7e42908a3b7db20a3c7c9a4ca0b1787a4
Anna Jacobsen <annagjacobsen@gmail.com>
2020-08-03 22:00:16 -0500

    aug 3

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'annagjacobsen@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'annaj@hey.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Message from  $name";
 $email_body = "$name sent you a message via the contact form on annajacobsen.com.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: annagjacobsen@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: anna@youcanbook.me\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
